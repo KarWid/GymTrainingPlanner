1991ae4e517df51d6b8b20130cfa80ae4b7621ba
Karol Widła <karwid94@onet.eu>
2021-11-06 08:37:40 +0100

    Clean up empty directories and removed unnecessary projects

--- GymTrainingPlanner.Model/Dtos/V1/Account/LoginDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GymTrainingPlanner.Model.Dtos.V1.Account
-{
-    public class LoginDTO
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
